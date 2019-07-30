23047a4ccbc308102c557070139301c492895893
LauriKukko <lauri.kukkonen@gmail.com>
2019-07-30 15:15:52 +0300

    Message luotu

--- src/main/java/fi/academy/saunaback/message/MessageController.java
+++ src/main/java/fi/academy/saunaback/message/MessageController.java
@@ -1,6 +1,9 @@
 package fi.academy.saunaback.message;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +16,15 @@ public class MessageController {
         this.mrep = mrep;
     }
 
+    @GetMapping()
+    public Iterable<Message>getAllMessages(){
+        return mrep.findAll();
+    }
+
+    @PostMapping(value = "/addmessage")
+    public Message addMessage(@RequestBody Message m){
+        return mrep.save(m);
+    }
 //    @GetMapping()
 //    tähän kaikkien viestien haku
 
