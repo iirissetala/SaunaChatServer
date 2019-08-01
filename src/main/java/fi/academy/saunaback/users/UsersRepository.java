aa0ea481b655d8bb45f0c984b218f81d6fc0c1bc
iirissetala <iirissetala@hotmail.com>
2019-08-01 17:36:39 +0300

    kommentit ja tietokannan ddl-auto muutos noneksi

--- src/main/java/fi/academy/saunaback/users/UsersRepository.java
+++ src/main/java/fi/academy/saunaback/users/UsersRepository.java
@@ -2,7 +2,7 @@ package fi.academy.saunaback.users;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
+/**  Repositoria, jonka avulla saadaan käyttöön valmiit metodit. Iiris */
 @RepositoryRestResource
 public interface UsersRepository extends CrudRepository<Users, Long> {
 
